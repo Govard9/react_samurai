0ce874a4f95c550b6fcb25af93a59830afc56b1f
govard9 <web-supsu@yandex.ru>
2022-04-19 11:04:01 +0700

    18 lesson

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import './App.scss';
-import Header from './components/Header/Header';
-import Navbar from './components/Navbar/Navbar';
-import Profile from './components/Profile/Profile';
+import "./App.scss";
+import Dialogs from "./components/Dialogs/Dialogs";
+import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
+import Profile from "./components/Profile/Profile";
 
-function App() {
+function App(props) {
 	return (
-		<div className='app-wrapper'>
+		<div className="app-wrapper">
 			<Header />
 			<Navbar />
-			<Profile />
+			<div class="app-wrapper-content">
+				<Dialogs />
+			</div>
+			{/* <Profile /> */}
 		</div>
 	);
 }
- 
+
 export default App;
--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -4,7 +4,7 @@ import s from "./Profile.module.scss";
 
 const Profile = () => {
 	return (
-		<div className={s.content}>
+		<div>
 			<div className={s.profileTopImg}>
 				<img src="https://bigpicture.ru/wp-content/uploads/2013/03/Panorama03.jpg" />
 			</div>
