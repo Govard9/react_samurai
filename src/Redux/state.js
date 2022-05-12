fae9bce6d566b3545582ec07005503d08b278ee5
govard9 <web-supsu@yandex.ru>
2022-05-12 11:00:42 +0700

    37 lesson

--- src/Redux/state.js
+++ src/Redux/state.js
@@ -1,58 +1,60 @@
-let rerenderEntireTree = () => {
-	console.log('State changed');
-}
-
-let state = {
-	profilePage: {
-		posts: [
-			{ id: 0, message: "Hi, how are you?", like: 23 },
-			{ id: 1, message: "My first post.", like: 4 },
-		],
-		newPostText: ''
+let store = {
+	_state: {
+		profilePage: {
+			posts: [
+				{ id: 0, message: "Hi, how are you?", like: 23 },
+				{ id: 1, message: "My first post.", like: 4 },
+			],
+			newPostText: ''
+		},
+		dialogsPage: {
+			dialogs: [
+				{ id: 1, name: "Dimych" },
+				{ id: 2, name: "Andrey" },
+				{ id: 3, name: "Sveta" },
+				{ id: 4, name: "Sasha" },
+				{ id: 5, name: "Viktor" },
+				{ id: 6, name: "Valery" },
+			],
+			messages: [
+				{ id: 1, message: "Hi", myMsg: 1},
+				{ id: 2, message: "How are you?", myMsg: 0},
+				{ id: 3, message: "qq", myMsg: 1},
+				{ id: 4, message: "Try hard", myMsg: 0},
+				{ id: 5, message: "Hello my friend", myMsg: 1},
+				{ id: 6, message: "Good idea!", myMsg: 0},
+			],
+			friends: [
+				{ id: 2, name: "Andrey" },
+				{ id: 4, name: "Sasha" },
+				{ id: 3, name: "Sveta" },
+			],
+		},
 	},
-	dialogsPage: {
-		dialogs: [
-			{ id: 1, name: "Dimych" },
-			{ id: 2, name: "Andrey" },
-			{ id: 3, name: "Sveta" },
-			{ id: 4, name: "Sasha" },
-			{ id: 5, name: "Viktor" },
-			{ id: 6, name: "Valery" },
-		],
-		messages: [
-			{ id: 1, message: "Hi", myMsg: 1},
-			{ id: 2, message: "How are you?", myMsg: 0},
-			{ id: 3, message: "qq", myMsg: 1},
-			{ id: 4, message: "Try hard", myMsg: 0},
-			{ id: 5, message: "Hello my friend", myMsg: 1},
-			{ id: 6, message: "Good idea!", myMsg: 0},
-		],
-		friends: [
-			{ id: 2, name: "Andrey" },
-            { id: 4, name: "Sasha" },
-			{ id: 3, name: "Sveta" },
-		],
+	getState() {
+		return this._state;
 	},
-};
-
-export let addPost = () => {
-	let newPost = {
-		id: 5,
-		message: state.profilePage.newPostText,
-		likesCount: 0
-	};
-
-	state.profilePage.posts.push(newPost);
-	state.profilePage.newPostText = '';
-	rerenderEntireTree();
+	_callSubscriber() {
+		console.log('State changed');
+	},
+	addPost() {
+		let newPost = {
+			id: 5,
+			message: this._state.profilePage.newPostText,
+			likesCount: 0
+		};
+	
+		this._state.profilePage.posts.push(newPost);
+		this._state.profilePage.newPostText = '';
+		this._callSubscriber(this._state);
+	},
+	updateNewPostText(newText) {
+		this._state.profilePage.newPostText = newText;
+		this._callSubscriber(this._state);
+	},
+	subscribe(observer) {
+		this._callSubscriber = observer;
+	}
 }
-export let updateNewPostText = (newText) => {
-	state.profilePage.newPostText = newText;
-	rerenderEntireTree();
-}
 
-export const subscribe = (observer) => {
-	rerenderEntireTree = observer;
-}
-
-export default state;
+export default store;
