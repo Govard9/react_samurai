5831205ef95b5da5945f03efd1338ae595c5986b
govard9 <web-supsu@yandex.ru>
2022-05-14 15:42:20 +0700

    42 lesson.

--- src/Redux/dialogs-reducer.js
+++ src/Redux/dialogs-reducer.js
@@ -1,4 +1,29 @@
-export const dialogReducer = (state, action) => {
+let initialState = {
+	dialogs: [
+		{ id: 1, name: "Dimych" },
+		{ id: 2, name: "Andrey" },
+		{ id: 3, name: "Sveta" },
+		{ id: 4, name: "Sasha" },
+		{ id: 5, name: "Viktor" },
+		{ id: 6, name: "Valery" },
+	],
+	messages: [
+		{ id: 1, message: "Hi", myMsg: 1 },
+		{ id: 2, message: "How are you?", myMsg: 0 },
+		{ id: 3, message: "qq", myMsg: 1 },
+		{ id: 4, message: "Try hard", myMsg: 0 },
+		{ id: 5, message: "Hello my friend", myMsg: 1 },
+		{ id: 6, message: "Good idea!", myMsg: 0 },
+	],
+	newMessage: "",
+	friends: [
+		{ id: 2, name: "Andrey" },
+		{ id: 4, name: "Sasha" },
+		{ id: 3, name: "Sveta" },
+	],
+};
+
+const dialogReducer = (state = initialState, action) => {
 	switch (action.type) {
 		case "ADD-MESSAGE":
 			let newMessage = {
@@ -13,14 +38,15 @@ export const dialogReducer = (state, action) => {
 		case "UPDATE_NEW_MESSAGE_TEXT":
 			state.newMessage = action.newText;
 			return state;
-        default:
-            return state;
+		default:
+			return state;
 	}
 };
 
-
-export const addMessageActionCreator = () => ({type: 'ADD-MESSAGE'});
+export const addMessageActionCreator = () => ({ type: "ADD-MESSAGE" });
 
 export const updateNewMessageTextActionCreator = (text) => {
-	return {type: 'UPDATE_NEW_MESSAGE_TEXT', newText: text };
-}
+	return { type: "UPDATE_NEW_MESSAGE_TEXT", newText: text };
+};
+
+export default dialogReducer;
--- src/Redux/profile-reducer.js
+++ src/Redux/profile-reducer.js
@@ -1,4 +1,12 @@
-export const profileReducer = (state, action) => {
+let initialState = {
+	posts: [
+		{ id: 0, message: "Hi, how are you?", like: 23 },
+		{ id: 1, message: "My first post.", like: 4 },
+	],
+	newPostText: "",
+};
+
+const profileReducer = (state = initialState, action) => {
 	switch (action.type) {
 		case "ADD-POST":
 			let newPost = {
@@ -13,14 +21,15 @@ export const profileReducer = (state, action) => {
 		case "UPDATE_NEW_POST_TEXT":
 			state.newPostText = action.newText;
 			return state;
-        default:
-            return state;
+		default:
+			return state;
 	}
 };
 
-
-export const addPostActionCreator = () => ({type: 'ADD-POST'});
+export const addPostActionCreator = () => ({ type: "ADD-POST" });
 
 export const updateNewPostTextActionCreator = (text) => {
-	return {type: 'UPDATE_NEW_POST_TEXT', newText: text };
-}
+	return { type: "UPDATE_NEW_POST_TEXT", newText: text };
+};
+
+export default profileReducer;
